931a36438691e85e27619b59e7120601617cf610
majd <majdshammout@hotmail.com>
2024-09-08 22:50:59 -0400

    Implement active link highlighting in MainNav component

--- components/demo-dashboard/main-nav.tsx
+++ components/demo-dashboard/main-nav.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 
@@ -7,6 +8,8 @@ export function MainNav({
   className,
   ...props
 }: React.HTMLAttributes<HTMLElement>) {
+  const pathname = usePathname();
+
   return (
     <nav
       className={cn("flex items-center justify-between w-full", className)}
@@ -14,14 +17,20 @@ export function MainNav({
     >
       <div className="flex items-center space-x-4 lg:space-x-6">
         <Link
-          href="app"
-          className="text-sm font-medium transition-colors hover:text-primary"
+          href="/app"
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-primary",
+            pathname === "/app" ? "text-primary" : "text-muted-foreground"
+          )}
         >
           Overview
         </Link>
         <Link
-          href="resume-reader"
-          className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+          href="/resume-reader"
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-primary",
+            pathname === "/resume-reader" ? "text-primary" : "text-muted-foreground"
+          )}
         >
           Upload Resume
         </Link>
@@ -32,7 +41,7 @@ export function MainNav({
           Upload Info
         </Link>
       </div>
-      <Link href="interview">
+      <Link href="/interview">
         <Button>Interview Now</Button>
       </Link>
     </nav>
