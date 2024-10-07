5f72603beafe2656b5d03bc12274d70fd1ef2974
mrodgers616 <mrodgers616@gmail.com>
2024-10-07 19:51:06 -0400

    using ngrok to run server locally

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.prepare().then(() => {
   const wss = new WebSocketServer({
     noServer: true,
     verifyClient: (info, callback) => {
-      const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', 'https://nextjs-server-897fb9f8b4e1.herokuapp.com/'];
+      const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', 'https://6ab0-2601-c2-1b81-4e0-e531-72e8-9f72-ef0c.ngrok-free.app'];
       if (allowedOrigins.includes(info.origin) || info.origin.endsWith('.herokuapp.com')) {
         callback(true);
       } else {
