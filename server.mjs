208e0ad217402760c635d02a6f5b9b9de8d13e8d
mrodgers616 <mr4683@nyu.edu>
2024-10-07 22:05:54 -0400

    fixed ngrok url

--- server.mjs
+++ server.mjs
@@ -29,7 +29,7 @@ app.prepare().then(() => {
   const wss = new WebSocketServer({
     noServer: true,
     verifyClient: (info, callback) => {
-      const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', 'https://03a9-2601-c2-1b81-4e0-58ce-e8c0-5e7c-9b9.ngrok-free.app'];
+      const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', 'https://03a9-2601-c2-1b81-4e0-58ce-e8c0-5e7c-9b9.ngrok-free.app', 'http://73.43.185.216:3001'];
       if (allowedOrigins.includes(info.origin) || info.origin.endsWith('.herokuapp.com')) {
         callback(true);
       } else {
